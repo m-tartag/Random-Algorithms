135c9975ed468eb22333d4ea9c816c24162c80ed
m-tartag <mattbrs51@gmail.com>
2019-09-26 11:27:40 -0400

    update

--- Inter-FCC/wherefore.js
+++ Inter-FCC/wherefore.js
@@ -1,10 +1,11 @@
 function whatIsInAName(collection, source) {
   // What's in a name?
-  const arr = [];
-  // Only change code below this line
-
-  // Only change code above this line
-  console.log(collection[1].hasOwnProperty('last'));
+  const ARR = collection.forEach(item => {
+    if (item.hasOwnProperty('last') && item.hasOwnValue('Capulet')) {
+      return true;
+    }
+  });
+  return ARR;
 }
 
 whatIsInAName(
