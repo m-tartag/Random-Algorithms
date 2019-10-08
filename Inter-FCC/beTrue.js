dce725bebd1b8b593cafd17f42dd92d5a10965d0
m-tartag <mattbrs51@gmail.com>
2019-10-08 11:51:17 -0400

    update

--- Inter-FCC/beTrue.js
+++ Inter-FCC/beTrue.js
@@ -12,7 +12,10 @@ console.log(
     [
       { user: 'Tinky-Winky', sex: 'male' },
       { user: 'Dipsy', sex: 'male' },
-      { user: 'Laa-Laa', sex: undefined },
+      {
+        user: 'Laa-Laa',
+        sex: 'female',
+      },
       { user: 'Po', sex: 'female' },
     ],
     'sex'
